7c4602ec3fee3ac979e668c755c04382e71b2c07
Abolfazl-Mousavi <99834995+Abolfazl-Mousavi@users.noreply.github.com>
2024-09-25 19:08:45 +0330

    Updated resume link

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -9,10 +9,7 @@ const page = () => {
         className="absolute bottom-0 right-0 mx-5 my-3 px-2 py-1 bg-blue-700 underline text-white rounded-full"
         href={resume}
       >view on docs</Link>
-      <iframe
-        className="w-[595px] h-screen"
-        src="https://docs.google.com/document/d/e/2PACX-1vTJib6seo23S2hlFZXpeQfw7Wa2TfsXNkSI9XfODd1wgZElp7haiHgt45CIrZLBXDzBIo_mQkZRUnyP/pub?embedded=true"
-      ></iframe>
+      <iframe  className="w-full h-screen" src="https://docs.google.com/document/d/1qMKlJpVVjXAu1zZKEl76ZIj_3NGIgoaxrETf1_zzbxc/pub?embedded=true"></iframe>
     </div>
   );
 };
